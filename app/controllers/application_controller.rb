544200eb2cf5cd9fc4b191295df88016915494f8
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-24 11:40:34 -0800

    add search form and user admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,18 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id])
     end
   end
-  
+
   def authorize
     if !current_user
-      flash[:alert] = "You aren't authorized to visit that page."
-      redirect_to '/'
+      flash[:alert] = "You need to log in."
+    end
+  end
+
+  def user_admin
+    if !current_user
+      flash[:alert] = "You are not logged in."
+    elsif current_user.admin = false
+      flash[:alert] = "You need to be an admin to perform that action."
     end
   end
 
